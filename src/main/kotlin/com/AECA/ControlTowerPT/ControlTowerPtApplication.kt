ecd2118133b35eded48dede5807791f365c81595
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-04-24 11:58:40 -0300

    first commit - Sprint added

---
+++ src/main/kotlin/com/AECA/ControlTowerPT/ControlTowerPtApplication.kt
@@ -0,0 +1,11 @@
+package com.AECA.ControlTowerPT
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControlTowerPtApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControlTowerPtApplication>(*args)
+}
---
+++ src/test/kotlin/com/AECA/ControlTowerPT/ControlTowerPtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.AECA.ControlTowerPT
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControlTowerPtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
