1bd8149aaeebcd7dd9eebb132e6788ca233732b5
Juan Martin Giorgio <67115848+GiorgioJuan@users.noreply.github.com>
2024-06-05 12:39:15 -0300

    loader working

--- src/main/kotlin/com/controltowerpt/loader/WarehouseLoader.kt
+++ src/main/kotlin/com/controltowerpt/loader/WarehouseLoader.kt
@@ -12,7 +12,7 @@ class WarehouseLoader(private val warehouseRepository: WarehouseRepository) : Co
     }
 
     private fun createWarehouse() {
-        if (warehouseRepository.findById(1).isEmpty) {
+        if (warehouseRepository.findAll().isEmpty()) {
             val warehouse = Warehouse()
             warehouse.direction = "Avenida simpatica de tuquito 123"
             warehouseRepository.save(warehouse)
