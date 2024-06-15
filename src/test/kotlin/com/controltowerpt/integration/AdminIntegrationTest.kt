30da4d6bd9f991c7ae2fbfa2af1f5d4c072710ec
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-06-14 22:56:37 -0300

    -gitignore updated

--- src/test/kotlin/com/controltowerpt/integration/AdminIntegrationTest.kt
+++ src/test/kotlin/com/controltowerpt/integration/AdminIntegrationTest.kt
@@ -1,10 +1,37 @@
 package com.controltowerpt.integration
 
+import com.controltowerpt.controllers.AdminController
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @SpringBootTest
 @ActiveProfiles("integration")
-@Transactional
-class AdminIntegrationTest
+@AutoConfigureMockMvc
+class AdminIntegrationTest{
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun test001createProductSuccessfully() {
+        val name = "Test Product"
+        val price = 10.0
+        val jsonBody = """{"name": "$name", "price": $price}"""
+
+        mockMvc.perform(
+            post("/shop/product/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody),
+        )
+            .andExpect(status().isOk)
+    }
+}
